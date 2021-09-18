96c2af6b5b07d1680acec22636ee2b65f19b6a1a
pkhirapara <pkhirapara@gmail.com>
2021-09-18 23:49:17 +0200

    adding CSS and Login Controller, Migration and Model created
    :

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+
+class LoginController extends Controller
+{
+    public function index(){
+
+        return view('login');
+
+    }
+
+    public function store(){
+
+        $user = new Login;
+        $user->username = Str::random(10); 
+        $user->save();
+
+        return redirect('/todo');
+
+    }
+}
